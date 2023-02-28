d14f74fa4a739b85ba7f7f04823707b583e450d6
Your Name Comes Here <vic20011233@gmail.com>
2023-02-28 20:30:02 +0100

    a

--- Commands/Public/last.js
+++ Commands/Public/last.js
@@ -6,6 +6,6 @@ module.exports = {
     .setDescription('enviare la ultima foto añadida'),
 
     execute(interaction) {
-        interaction.reply('https://cdn.discordapp.com/attachments/1079901942303694918/1080208333052575795/image.png', 'https://cdn.discordapp.com/attachments/1079901942303694918/1080208524929413150/image.png')
+        interaction.reply('https://cdn.discordapp.com/attachments/1079901942303694918/1080209999374057502/image.png')
     }
 }
