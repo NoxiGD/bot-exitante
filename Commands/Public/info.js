0c822cd724303b209b41273800a2152bd469dc22
Your Name Comes Here <vic20011233@gmail.com>
2023-03-01 15:36:11 +0100

    sdcdd

--- Commands/Public/info.js
+++ Commands/Public/info.js
@@ -8,7 +8,7 @@ module.exports = {
     execute(interaction) {
         const embed = new EmbedBuilder()
         .setTitle('Pablus Bot')
-        .setDescription('hola! soy pablus bot y mi especialidad es enviar capturas graciosas de pabluskino puedes usar **/last** para ver la ultima captura que fue añadida o bien usar **/pablus** para ver una captura de pablus randomizada, si te interesa ver memes usa **/meme**, disfruta!')
+        .setDescription('hola! soy pablus bot y mi especialidad es enviar capturas graciosas de pabluskino puedes usar **/help** para saber que comandos tengo o bien usar **/pablus** para ver una captura de pablus randomizada, si te interesa ver memes usa **/meme**, disfruta!')
         .setFooter({text: '⚠️ Atención: todas las capturas son falsas o sacadas de contexto, este bot no tiene el fin de burlarse ni manchar su imagen, es solo humor absurdo.'})
         .setColor('Blurple')
 
