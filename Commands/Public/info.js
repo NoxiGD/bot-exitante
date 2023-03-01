b7a3b881d1848629b4c47e3488fd049642bcf94e
Your Name Comes Here <vic20011233@gmail.com>
2023-03-01 12:44:44 +0100

    s

---
+++ Commands/Public/info.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('te enseñare que puedes hacer conmigo'),
+
+    execute(interaction) {
+        const embed = new EmbedBuilder()
+        .setTitle('Pablus Bot')
+        .setDescription('hola! soy pablus bot y mi especialidad es enviar capturas graciosas de pabluskino puedes usar **/last** para ver la ultima captura que fue añadida o bien usar **/pablus** para ver una captura de pablus randomizada disfruta!')
+        .setFooter(`⚠️ Atención: todas las capturas son falsas o sacadas de contexto, este bot no tiene el fin de burlarse ni manchar su imagen, es solo humor absurdo`)
+        .setColor('Blurple')
+
+        interaction.reply({ embeds: [embed]})
+    },
+};
