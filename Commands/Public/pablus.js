3e46e84d933ee7ee3eecb9d2a41bfa4a6e2bc291
Your Name Comes Here <vic20011233@gmail.com>
2023-03-01 11:37:15 +0100

    s

--- Commands/Public/pablus.js
+++ Commands/Public/pablus.js
@@ -16,7 +16,8 @@ module.exports = {
     "https://cdn.discordapp.com/attachments/1079901942303694918/1080177805184204830/61_sin_titulo_20230228141700.png", "https://cdn.discordapp.com/attachments/1079901942303694918/1080161486749565009/image.png", "https://cdn.discordapp.com/attachments/1079901942303694918/1080161119202717926/image.png", "https://cdn.discordapp.com/attachments/1079901942303694918/1080160669967589446/image.png", "https://cdn.discordapp.com/attachments/1079901942303694918/1080200717454807141/image.png", "https://cdn.discordapp.com/attachments/1069349177588453527/1080204814597759087/image.png", "https://cdn.discordapp.com/attachments/1079901942303694918/1080206128576737311/image.png", "https://cdn.discordapp.com/attachments/1079901942303694918/1080208333052575795/image.png", "https://cdn.discordapp.com/attachments/1079901942303694918/1080208524929413150/image.png",  "https://cdn.discordapp.com/attachments/1079901942303694918/1080209999374057502/image.png"]
 
         const ball = Math.round((Math.random() * choice.length))
-            await interaction.reply (`${choice[ball]} ⚠️ Atención: todas las capturas son falsas o sacadas de contexto, este bot no tiene el fin de burlarse ni manchar su imagen, es solo humor absurdo`).catch(err =>{
+            await interaction.reply (`${choice[ball]}
+             ⚠️ Atención: todas las capturas son falsas o sacadas de contexto, este bot no tiene el fin de burlarse ni manchar su imagen, es solo humor absurdo`).catch(err =>{
 
             })
   },
