f30463a5f955b02bb41ed30f532e35aa0653867c
Your Name Comes Here <vic20011233@gmail.com>
2023-03-01 12:32:54 +0100

    a

--- Commands/Public/last.js
+++ Commands/Public/last.js
@@ -6,7 +6,7 @@ module.exports = {
     .setDescription('enviare la ultima foto añadida'),
 
     execute(interaction) {
-        interaction.reply(`https://cdn.discordapp.com/attachments/1079901942303694918/1080449646092103690/image.png
-        ⚠️ Atención: todas las capturas son falsas o sacadas de contexto, este bot no tiene el fin de burlarse ni manchar su imagen, es solo humor absurdo`)
+        interaction.reply(`https://cdn.discordapp.com/attachments/1079901942303694918/1080452430690533457/image.png
+⚠️ Atención: todas las capturas son falsas o sacadas de contexto, este bot no tiene el fin de burlarse ni manchar su imagen, es solo humor absurdo`)
     },
 };
