63a2edbd705cf087808c0cad9cd0e4f3be18142d
Your Name Comes Here <vic20011233@gmail.com>
2023-03-01 11:31:51 +0100

    s

--- Commands/Public/test.js
+++ Commands/Public/test.js
@@ -8,7 +8,8 @@ module.exports = {
     execute(interaction) {
         const embed = new EmbedBuilder()
         .setTitle('pablus')
-        .setDescription('https://cdn.discordapp.com/attachments/1079901942303694918/1080209999374057502/image.png')
+        .setDescription('test')
+        .setImage('https://cdn.discordapp.com/attachments/1079901942303694918/1080209999374057502/image.png')
         .setFooter(' ⚠️ Atención: todas las capturas son falsas o sacadas de contexto, este bot no tiene el fin de burlarse ni manchar su imagen, es solo humor absurdo')
 
         interaction.reply({ embeds: [embed]})
