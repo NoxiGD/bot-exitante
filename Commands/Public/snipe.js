b9c5ca07ac2a8243ad9c3f4c7d531f863d429e73
Your Name Comes Here <vic20011233@gmail.com>
2023-03-01 18:46:55 +0100

    asfd

---
+++ Commands/Public/snipe.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { EmbedBuilder } = require("discord.js");
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("snipe")
+    .setDescription("Snipes the last deleted message from the current channel"),
+    async execute (interaction, client) {
+
+        const msg = client.snipes.get(interaction.channel.id);
+        if (!msg) return await interaction.reply({ content: "I cant find any deleted messages", ephemeral: true});
+
+        const ID = msg.author.id;
+        const member = interaction.guild.members.cache.get(ID)
+        const URL = member.displayAvatarURL();
+
+        const embed = new EmbedBuilder()
+        .setColor("Blue")
+        .setTitle(`Sniped message - ${member.user.tag}`)
+        .setDescription(`${msg.content}`)
+        .setTimestamp()
+        .setFooter({ text: `Member ID: ${ID}`, iconURL: `${URL}`})
+
+        if (msg.image) embed.setImage(msg.image)
+        await interaction.reply({ embeds: [embed] })
+
+    },
+};
--- index.js
+++ index.js
@@ -63,6 +63,14 @@ client.on("interactionCreate", async (interaction) => {
     name: 'Dash History'
     });
   });
+  client.snipes = new Map()
+client.on(`messageDelete`, function(message, channel) {
+  client.snipes.set(message.channel.id, {
+    content: message.content,
+    author: message.author,
+    image: message.attachments.first() ? message.attachments.first().proxyURL : null
+  });
+});
   client.config = require("./config.json");
   client.events = new Collection();
   client.commands = new Collection();
