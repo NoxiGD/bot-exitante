2b28ba1f758c2f9ba09c7ca27b0f01dd266e6c38
Your Name Comes Here <vic20011233@gmail.com>
2023-03-01 12:16:39 +0100

    s

--- Commands/Public/last.js
+++ Commands/Public/last.js
@@ -6,7 +6,7 @@ module.exports = {
     .setDescription('enviare la ultima foto añadida'),
 
     execute(interaction) {
-        interaction.reply(`https://cdn.discordapp.com/attachments/1079901942303694918/1080209999374057502/image.png
+        interaction.reply(`https://cdn.discordapp.com/attachments/1079901942303694918/1080448213095563345/image.png
         ⚠️ Atención: todas las capturas son falsas o sacadas de contexto, este bot no tiene el fin de burlarse ni manchar su imagen, es solo humor absurdo`)
     },
 };
